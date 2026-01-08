750fc3129daa336d91126de0d2bdc6c11e40fdb6
Guillermo Pattenden Lorenzo <guikke.pattenden@gmail.com>
2026-01-08 19:25:52 +0100

    Button Id has changed

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,4 +1,4 @@
-const contactBtn = document.getElementById("contactBtn");
+const contactBtn = document.getElementById("hero-button");
 
 contactBtn.addEventListener("click", () => {
   alert("You can contact me at: your.email@example.com");
