0a6b0b68faaf3b1da1093a42453cfb4c0ac162c0
Guillermo Pattenden Lorenzo <guikke.pattenden@gmail.com>
2026-01-08 19:27:12 +0100

    The button is a class not an Id

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,4 +1,4 @@
-const contactBtn = document.getElementById("hero-button");
+const contactBtn = document.getElementByClass("hero-button");
 
 contactBtn.addEventListener("click", () => {
   alert("You can contact me at: your.email@example.com");
